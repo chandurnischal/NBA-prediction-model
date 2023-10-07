ea84405529213917b15424de57e05c9a6b4fcd73
warlock7867 <chandur.nischal@gmail.com>
2023-10-07 15:51:18 -0400

    initial push

--- a.py
+++ a.py
@@ -1,1 +1,34 @@
-print("Hello world")
+import requests
+from bs4 import BeautifulSoup
+from os.path import basename, join
+from tqdm import tqdm 
+
+class NBA:
+    def __init__(self) -> None:
+        self.baseURL = "https://www.basketball-reference.com/"
+        self.regular = self.baseURL + "leagues"
+        self.playoffs = self.baseURL + "playoffs"
+    
+    def scrapeYear(self, type:str, year:int, destination:str) -> None:
+
+        if type == "regular":
+            url = self.regular
+        elif type == "playoffs":
+            url = self.playoffs
+        else:
+            return None
+
+        url += "/NBA_{}_totals.html".format(year)
+
+        r = requests.get(url=url)
+
+        with open(join(destination, basename(url)), "w", encoding="utf-8") as f:
+            f.write(r.text)
+
+        
+    def scrapeYears(self, type:str, startYear:int, endYear:int, destination:str) -> None:
+        for year in tqdm(range(startYear, endYear + 1)):
+            self.scrapeYear(type, year, destination)
+
+
+nba = NBA()
