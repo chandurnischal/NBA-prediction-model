e2da95565d1e8f69d49d73f5f56874714a148e3f
warlock7867 <chandur.nischal@gmail.com>
2023-10-08 16:13:41 -0400

    updated class Teams
    added new functions and fixed older bugs

--- extraction/extraction.py
+++ extraction/extraction.py
@@ -1,7 +1,7 @@
 import requests
 from os.path import join
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 
 
 class Players:
@@ -77,7 +77,6 @@ class Players:
 
         # create url for the page
         url += "/NBA_{}_totals.html".format(year)
-        # url += "/NBA_{}_per_game.html".format(year)
         # request html
         r = requests.get(url)
 
@@ -85,7 +84,6 @@ class Players:
 
         # locate table in html
         table = soup.find("table", id="totals_stats")
-        # table = soup.find("table", id = "per_game_stats")
 
         data = self.__tableExtractor(table)
 
@@ -169,29 +167,34 @@ class Players:
 
 
 class Teams:
-    def __init__(self) -> None:
+    def __init__(self, type: str) -> None:
         self.baseURL = "https://www.basketball-reference.com/"
-        self.regular = join(self.baseURL, "leagues")
-        self.playoffs = join(self.baseURL, "playoffs")
+
+        if type == "regular":
+            self.baseURL += "leagues/"
+        elif type == "playoffs":
+            self.baseURL += "playoffs/"
+        else:
+            return None
 
     def __tableExtractor(self, table) -> pd.DataFrame:
         columns = table.find("thead").text.strip().split(" ")
-        remove = table.find_all("tr", class_="thead")
+
+        tbody = table.find("tbody")
+
+        remove = tbody.find_all("tr", class_="thead")
 
         for r in remove:
             r.decompose()
 
-        tbody = table.find("tbody")
-
         tableRows = tbody.find_all("tr")
-
         rows = []
-
-        for tr in tableRows:
+        for tableRow in tableRows:
             row = []
-            th = tr.find("th").text
-            td = tr.find_all("td")
-            row.append(th)
+            th = tableRow.find("th")
+            row.append(th.text)
+            td = tableRow.find_all("td")
+
             for t in td:
                 row.append(t.text)
             rows.append(row)
@@ -200,53 +203,78 @@ class Teams:
 
         return data
 
-    def totalStats(self, type: str, year: int) -> pd.DataFrame:
-        if type == "regular":
-            url = self.regular
-        elif type == "playoffs":
-            url = self.playoffs
-        else:
-            return None
-
-        url += "/NBA_{}.html".format(year)
+    def totalStats(self, year: int) -> pd.DataFrame:
+        url = self.baseURL + "NBA_{}.html".format(year)
 
         r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
 
-        soup = BeautifulSoup(r.text, "html.parser")
         table = soup.find(id="totals-team")
 
-        return self.__tableExtractor(table)
+        data = self.__tableExtractor(table)
+        data["Year"] = year
+        return data
 
-    def perGameStats(self, type: str, year: int) -> pd.DataFrame:
-        if type == "regular":
-            url = self.regular
-        elif type == "playoffs":
-            url = self.playoffs
-        else:
-            return None
-
-        url += "/NBA_{}.html".format(year)
+    def perGameStats(self, year: int) -> pd.DataFrame:
+        url = self.baseURL + "NBA_{}.html".format(year)
 
         r = requests.get(url)
 
         soup = BeautifulSoup(r.text, "html.parser")
         table = soup.find(id="per_game-team")
+        data = self.__tableExtractor(table)
+        data["Year"] = year
+        return data
 
-        return self.__tableExtractor(table)
-
-    def perPossession(self, type: str, year: int) -> pd.DataFrame:
-        if type == "regular":
-            url = self.regular
-        elif type == "playoffs":
-            url = self.playoffs
-        else:
-            return None
-
-        url += "/NBA_{}.html".format(year)
+    # doesn't work. potential problem with website
+    def perPossStats(self, year: int) -> pd.DataFrame:
+        url = self.baseURL + "NBA_{}.html".format(year)
 
         r = requests.get(url)
 
-        soup = BeautifulSoup(r.text, "html.parser")
-        table = soup.find(id="per_poss-team")
+        soup = BeautifulSoup(r.content, "lxml")
+        soup = BeautifulSoup("\n".join(soup.find_all(string=Comment)), "lxml")
+        data = pd.read_html(url)
 
-        return self.__tableExtractor(table)
+        for d in data:
+            print(
+                d,
+                "\n------------------------------------------------------------------------------\n",
+            )
+
+        # print(soup)
+        # return self.__tableExtractor(table)
+
+    def extendedStandings(self, year: int) -> pd.DataFrame:
+        url = self.baseURL + "NBA_{}_standings.html".format(year)
+
+        r = requests.get(url)
+
+        soup = BeautifulSoup(r.text, "lxml")
+        soup = BeautifulSoup("\n".join(soup.find_all(string=Comment)), "lxml")
+        table = soup.find(id="expanded_standings")
+        thead = table.find("thead")
+        trs = thead.find_all("tr")
+        smallCols = trs[1].text.strip().split("\n")
+        tbody = table.find("tbody")
+
+        tableRows = tbody.find_all("tr")
+
+        rows = []
+
+        for tableRow in tableRows:
+            row = []
+
+            row.append(tableRow.find("th").text.strip())
+
+            tds = tableRow.find_all("td")
+
+            for td in tds:
+                row.append(td.text.strip())
+
+            rows.append(row)
+
+        data = pd.DataFrame(rows, columns=smallCols)
+        data["Year"] = year
+
+        return data
