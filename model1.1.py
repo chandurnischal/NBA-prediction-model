a7dc2dd6fcedc1a15e35d8d95f2f1c9209a1cce5
Velaga Rahul <146791085+Velaga-Rahul@users.noreply.github.com>
2023-11-10 14:59:28 -0500

    Add files via upload
    Signed-off-by: Velaga Rahul <146791085+Velaga-Rahul@users.noreply.github.com>

---
+++ model1.1.py
@@ -0,0 +1,56 @@
+import mysql.connector as mc
+import pandas as pd
+import numpy as np 
+
+creds = {
+    "host": "localhost", 
+    "password": "Chatgpt132",
+    "user": "root",
+    "database": "nba"
+}
+
+
+with mc.connect(**creds) as conn:
+    cur = conn.cursor()
+
+    query = '''select * from games;'''
+
+    cur.execute(query)
+
+    column_names = [column[0] for column in cur.description]
+    rows = cur.fetchall()
+
+data = pd.DataFrame(rows, columns = column_names)
+data.columns = [c.lower() for c in data.columns.to_list()]
+
+elo_ratings = {team: 1500 for team in set(data['home']) | set(data['visitor'])}
+
+elo_list = []
+homeelolist = []
+visitorelolist = []
+
+for index, row in data.iterrows():
+    home = row['home']
+    visitor = row['visitor']
+    home_elo = elo_ratings[home]
+    away_elo = elo_ratings[visitor]
+
+    E_home = 1 / (1+ 10 ** ((away_elo - home_elo) / 400))
+    E_away = 1 - E_home
+
+    k = 20 * ((abs(row['mov']) + 3) ** 0.8) / (7.5 + 0.006 * (abs(home_elo - away_elo)))
+    home_score = 1 if row['hpoints'] > row['vpoints'] else 0.5
+    away_score = 1 - home_score
+    new_home_elo = round(home_elo + k * (home_score - E_home), 2)
+    new_away_elo = round(away_elo + k * (away_score - E_away), 2)
+
+    elo_ratings[home] = new_home_elo
+    elo_ratings[visitor] = new_away_elo
+    homeelolist.append(new_home_elo)
+    visitorelolist.append(new_away_elo)
+
+data['home_elo'] = homeelolist
+data['visitor_elo'] = visitorelolist
+
+print(data)
+
