a1e3413622a180258a99faf87bbb51d974780d0f
Velaga Rahul <146791085+Velaga-Rahul@users.noreply.github.com>
2023-11-17 13:55:34 -0500

    Add files via upload
    Signed-off-by: Velaga Rahul <146791085+Velaga-Rahul@users.noreply.github.com>

--- shooting_percentage.py
+++ shooting_percentage.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 creds = {
-    "host": "localhost", 
+    "host": "localhost",
     "password": "Chatgpt132",
     "user": "root",
     "database": "nba"
@@ -30,7 +30,17 @@ data = pd.DataFrame(rows, columns=column_names)
 data['FGA_e'] = data['FGA'].replace(0,1)
 data['FTA_e'] = data['FTA'].replace(0,1)
 data['shooting_percenatge'] = data['PTS'].astype(float)/(data['FGA_e'].astype(float)*2*data['FTA_e'].astype(float)*2)
-print(data)
 
 
+# offensive rating =  100*points/possession
+#possession = FGA + 0.44*FTA + TOV
+# data['TOV_e'] = data['TOV'].replace(0,0.001)
 
+data['Offensive_rating'] = data['PTS'].astype(float) / ((data['FGA']).astype(float) + 0.44*data['FTA'].astype(float) + data['TOV'].astype(float))
+
+data['EFF'] = data['PTS'].astype(float) + data['TRB'].astype(float) + data['AST'].astype(float) + data['STL'].astype(float) + data['BLK'].astype(float) - data['FGA'].astype(float) + data['FG'].astype(float) - data['FTA'].astype(float) + data['FT'].astype(float) - data['TOV'].astype(float)
+
+
+data.to_csv('player_per_game_new.csv', index = False)
+
+#dqv.apply(pd.to_numeric, errors = 'ignore')
