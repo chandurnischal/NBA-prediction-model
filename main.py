3fc22bef44eb6baf010c8bf0d439fcd046f7f56b
chandurnischal <chandur.nischal@gmail.com>
2023-12-28 20:25:04 -0500

    qol

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    os.system("python processing/procees.py")
+    os.system("python processing/process.py")
     os.system("python app/app.py")
 
 if __name__ == "__main__":
