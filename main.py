b322b84a885bbb16cd0b3bab134b1ec327e08208
chandurnischal <chandur.nischal@gmail.com>
2023-12-28 20:06:10 -0500

    qol

---
+++ main.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+import sys
+sys.path.append('app/utils.py')
+import os
+import app.utils as u
+import json
+
--- processing/process.py
+++ processing/process.py
@@ -1,8 +1,31 @@
+from datetime import datetime, timedelta
 import os
 from time import perf_counter
+import utils as u
+import json
 
-start = perf_counter()
-os.system("python processing/load.py")
-os.system("python processing/clean.py")
+def updateDB():
 
-print("Execution Time: {} minutes".format(round((perf_counter() - start) / 60, 1)))
+    start = perf_counter()
+    os.system("python processing/load.py")
+    os.system("python processing/clean.py")
+
+    print("Execution Time: {} minutes".format(round((perf_counter() - start) / 60, 1)))
+
+yest = datetime.now() - timedelta(1)
+yest = datetime.strftime(yest, '%Y-%m-%d')
+
+with open('creds.json') as file:
+    creds = json.load(file)
+
+query = '''
+select * from games where date='{}' and vpoints != ''
+'''.format(yest)
+
+data = u.sqlTodf(query, creds)
+
+
+if data.empty:
+    updateDB()
+else:
+    print('Database up to date.')
