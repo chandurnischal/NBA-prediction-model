cdf9304380272cd63b0832d62ac89e7dd8ad15bc
chandurnischal <chandur.nischal@gmail.com>
2024-03-31 21:38:44 -0400

    changes to accuracy.py

--- analysis/accuracy.py
+++ analysis/accuracy.py
@@ -6,16 +6,17 @@ import seaborn as sns
 data = pd.read_excel("data/tracker.xlsx")
 data = data[["date", "acc"]]
 x = [i + 1 for i in data.index]
-median = np.median(data['acc'])
+mean = np.mean(data['acc'])
 
 lastIndex, lastElement = data.index[-1], data["acc"].iloc[-1]
 
 sns.set_style('darkgrid')
 plt.figure(figsize=(10, 7))
 sns.lineplot(x=x, y=data['acc'])
+plt.text(x=-25, y=mean, s='{}%'.format(round(mean, 1)), ha='left', va='bottom')
 plt.title("Cumulative Accuracy")
 plt.xlabel("Matches")
 plt.ylabel("Cumulative Accuracy")
-plt.axhline(y=median, c = 'k')
+plt.axhline(y=mean, c = 'k')
 plt.text(lastIndex, lastElement, "{}%".format(lastElement), ha="left", va="top")
 plt.show()
