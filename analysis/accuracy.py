af56627208477abf3327e2cb53864f3a4bdbce51
chandurnischal <chandur.nischal@gmail.com>
2024-04-07 10:54:35 -0400

    changed accuracy.py

--- analysis/accuracy.py
+++ analysis/accuracy.py
@@ -1,20 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
+import sys
 
-data = pd.read_excel("data/tracker.xlsx")
-data = data.dropna(subset=["outcome"])
-data = data[["date", "acc"]]
-x = [i + 1 for i in data.index]
+def plotLine(data:pd.DataFrame, n:int=-1):
+    data = data.dropna(subset=["outcome"])
+    length = len(data.index)
+    if n != -1 or n >= length:
+        data = data.tail(n)
+    data = data[["date", "acc"]]
+    x = [i + 1 for i in data.index]
 
-lastIndex, lastElement = data.index[-1], data["acc"].iloc[-1]
+    lastIndex, lastElement = data.index[-1], data["acc"].iloc[-1]
 
-sns.set_style('darkgrid')
-plt.figure(figsize=(10, 7))
-sns.lineplot(x=x, y=data['acc'])
-plt.title("Cumulative Accuracy")
-plt.xlabel("Matches")
-plt.ylabel("Cumulative Accuracy")
-plt.text(lastIndex, lastElement, "{}%".format(lastElement), ha="left", va="top")
-plt.show()
+    sns.set_style('darkgrid')
+    plt.figure(figsize=(10, 7))
+    sns.lineplot(x=x, y=data['acc'])
+    if n == -1 or n >= length:
+        plt.title("Cumulative Accuracy")
+    else:
+        plt.title("Cumulative Accuracy (Last {} Games)".format(n))
+
+    plt.xlabel("Matches")
+    plt.ylabel("Cumulative Accuracy")
+    plt.text(lastIndex, lastElement, "{}%".format(lastElement), ha="left", va="top")
+    plt.show()
+
+try:
+    n = int(sys.argv[1])
+    data = pd.read_excel("data/tracker.xlsx")
+    plotLine(data, n)
+except:
+    data = pd.read_excel("data/tracker.xlsx")
+    plotLine(data)
